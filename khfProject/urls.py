615920d94819d3f0b33d8f4b891141f9bfcb78a1
ORayy <asare.oanane@gmail.com>
2021-06-15 14:10:22 +0100

    added url path to main project urlpatterns

--- khfProject/urls.py
+++ khfProject/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('khfApp.urls', namespace='store'))
 ]
 
 # configuring/utilising MEDIA_URL and MEDIA_ROOT while we're in development area
