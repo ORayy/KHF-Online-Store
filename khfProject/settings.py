e7f01d566ae6450cca1a41d7fd5cafa0cfd05d0c
ORayy <asare.oanane@gmail.com>
2021-06-24 23:39:28 +0100

    refactored context_processor for khfProject/settings.py

--- khfProject/settings.py
+++ khfProject/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'khfApp.views.categories',
+                'khfApp.context_processors.categories',
             ],
         },
     },
